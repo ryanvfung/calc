65c054ea3f88ac621ca2ff5e7957841ab636ea4e
Ryan Fung <rvfung@gmail.com>
2015-04-05 23:19:16 +0100

    Calc can now add 2 numbers together

--- README.md
+++ README.md
@@ -9,7 +9,12 @@ Introduction
 
 **Created**: 2013-12-21
 
-**Last Modified**: 2013-12-21
+**Last Modified**: 2015-04-04
+
+
+Current Features
+----------------
+* Add 2 numbers together
 
 
 Upcoming Features to be Implemented
--- scicalc.js
+++ scicalc.js
@@ -10,30 +10,61 @@
 var baseUnits = ["kg", "m", "s", "A", "K", "mol", "cd"];
 var unitData = {}; // global units dataset
 var dimData = {}; // global Dimensions dataset
-var scicalcInputListener = $("#scicalcSubmit").addEventListener("click",parseInput);
 
-// Regular Expressions
+// Regular Expressions, keep search parameters
 var re = {
-	number: /[(0-9)]/g,
-	letter: /[(A-Za-z)]/g,
-	operator: /[(\+\-\*\/\=\(\))]/g
+	number: /([0-9])/g,
+	letter: /([A-Za-z])/g,
+	bracket: /([\(\)])/g,
+	operator: /([\+\-\*\/\=])/g
 };
-// Regular Expressions, discard search
+// Regular Expressions, discard search parameters
 var red = {
 	number: /[0-9]/g,
 	letter: /[A-Za-z]/g,
-	operator: /[\+\-\*\/\=\(\)]/g
+	bracket: /[\(\)]/g,
+	operator: /[\+\-\*\/\=]/g
 };
 
 window.onload=function(){
+	$('#scicalcSubmit').on('click', parseInput);
     var element = document.createElement("p");
-    element.innerHTML="<tt>scicalc.js</tt> successfully loaded!";
-    document.body.appendChild(element);
+    element.innerHTML = "<code>scicalc.js</code> successfully loaded!";
+    document.getElementById('scicalcOutput').appendChild(element);
     for(i in siUnits){
         var a = 0;
         for(j in siUnits[i][1]){a+=j}
         unitData[siUnits[i][0]] = siUnits[i][1];
     }
+}
+
+function add (num1, num2) {
+	// if (sameDimensions) {
+		// raiseDimensionError
+	// } else {
+		return Number(num1) + Number(num2);
+	// }
+}
+
+function parseInput(){ // parse input expression
+    var str = $("#scicalcInput")[0].value;
+    var exp = str.split(re.operator);
+	for (var i = 0; i < exp.length; i++) {
+		console.log(exp[i]);
+		if (exp[i] == "+" && i > 0 && i < exp.length) {
+			exp.splice(i-1, 3, add(exp[i-1], exp[i+1]));
+		}
+	}
+	if(exp.length != 1) {
+		// raiseError("");
+		console.log('exp.length:' + exp.length);
+		console.log(exp);
+	} else {
+		result = document.createElement("p");
+    	result.innerHTML = str + " = " + exp[0];
+		console.log(result.innerHTML);
+		document.getElementById('scicalcOutput').appendChild(result);
+	}
 }
 
 function makeNum(id, value, units){
@@ -45,10 +76,6 @@ function dim(value, units){ // new Dimension
     this.units = units;
 }
 
-function parseInput(){ // parse input expression
-    var str = $("#scicalcInput").value;
-    
-}
 function ungroup(s){ // extract expression from inside round brackets
     try { s=s.split("(")[1].split(")")[0]; }
     catch (e) {}
