67c43ceb778d727ee11a9d0da7090fb8c8c85764
Ryan Fung <rvfung@gmail.com>
2015-04-24 21:52:03 +0100

    Added basic support for 1 dimension quantities parsing

--- scicalc.js
+++ scicalc.js
@@ -1,13 +1,12 @@
-/*
- * Title:              SciCalc - Scientific Calculator Module
+/* Title:              SciCalc - Scientific Calculator Module
  * Version:            0.1
  * Author:             Ryan Fung
  * Date created:	   2014-04-16
  * Date last modified: 2015-04-21
  */
+/*global $, siUnits */
 
-
-window.onload = function() {
+$(document).ready(function () {
 	$('#scicalcSubmit').on('click', function () {
 		checkInput($('#scicalcInput').val());
 	});
@@ -17,14 +16,14 @@ window.onload = function() {
 	});
 	$('#scicalcInput').focus();
 	writeMessage('<code>scicalc.js</code> successfully loaded!');
-	for(i in siUnits){
+	for (var i in siUnits){
 		var a = 0;
-		for (j in siUnits[i][1]) {
+		for (var j in siUnits[i][1]) {
 			a += j;
 		}
 		unitData[siUnits[i][0]] = siUnits[i][1];
 	}
-}
+});
 
 var baseUnits = ['kg', 'm', 's', 'A', 'K', 'mol', 'cd'];
 var unitData = {}; // global units dataset
@@ -50,19 +49,24 @@ var red = {
 	p: /[pn\u03bcmcdhkMGT]/g // prefixes
 };
 
+// Regular Expressions, keep search parameters, single search
+var res = {
+	l: /([A-Za-z])/,  // letters
+};
+
 // Regular Expressions for operations
 var reo = {
 	1: /[\^]/g,
 	2: /[*\/]/g,
 	3: /[\+\-]/g
-}
+};
 
 function parseInput(exp){ // parse input expression
 
 	// perform priority 1 operations
 	for (var i = 1; i < exp.length - 1; i++) {
 		if (!(exp[i] instanceof qty)) {
-			if (exp[i].split(reo[1]).join('')=='') {
+			if (exp[i].split(reo[1]).join('')==='') {
 				// perform exponentiation
 				if (exp[i] == '^') {
 					exp.splice(i-1, 3, power(exp[i-1], exp[i+1]));
@@ -75,7 +79,7 @@ function parseInput(exp){ // parse input expression
 	// perform priority 2 operations
 	for (var i = 1; i < exp.length - 1; i++) {
 		if (!(exp[i] instanceof qty)) {
-			if (exp[i].split(reo[2]).join('')=='') {
+			if (exp[i].split(reo[2]).join('')==='') {
 				// perform multiplication
 				if (exp[i] == '*') {
 					exp.splice(i-1, 3, multiply(exp[i-1], exp[i+1]));
@@ -94,7 +98,7 @@ function parseInput(exp){ // parse input expression
 	for (var i = 1; i < exp.length - 1; i++) {
 //		console.log("exp[i].split(reo[1]).join('')  =  "+String(exp[i].split(reo[3]).join('')));
 		if (!(exp[i] instanceof qty)) {
-			if (exp[i].split(reo[3]).join('')=='') {
+			if (exp[i].split(reo[3]).join('')==='') {
 				// perform addition
 				if (exp[i] == '+') {
 					exp.splice(i-1, 3, add(exp[i-1], exp[i+1]));
@@ -129,26 +133,41 @@ function checkInput (input) {
 
 function processInput (input) {
 
-	// 287 J kg^-1 K^-1
+	// 287 J kg^-1 K^-1   =   qty(287, [1, 2, -2, 0, -1, 0, 0]);
+	// =
 	// num unit[^int] [*/ unit[^int]]
 	// num unit[^int] [* unit[^int]] [/  (unit[^int] [* unit[^int]])]
 	
 	var exp = input.split(re.nan);
 	
-	// remove extra array item at start if input string starts with something that is not a number
-	if (exp[0].split(re.nan).join('') === '' ) {
+	// remove extra array item at start if input string starts with whitespace
+	if (exp[0].replace(/\s/g,'') === '' ) {
 		exp.shift();
 	}
 	
-	// remove extra array item at end if input string ends with something that is not a number
-	if (exp[exp.length-1].split(re.nan).join('') === '' ) {
+	// remove extra array item at end if input string ends with whitespace
+	if (exp[exp.length-1].replace(/\s/g,'') === '' ) {
 		exp.pop();
 	}
 	
 	// convert numbers to object
 	for (var i = 0; i < exp.length; i++) {
 		if (!re.nan.test(exp[i])) {
-			exp[i] = new qty(Number(exp[i]), 0);
+			var split = exp[i].replace(/\s/g,'').split(res.l);
+			var units = [0, 0, 0, 0, 0, 0, 0];
+			if (split[0]===''){
+				split.shift();
+			}
+			if (split[split.length-1]===''){
+				split.pop();
+			}
+			for (var j = 1; j < split.length; j++) {
+				var array = siUnits[split[j]];
+				if ( array ) {
+					array.forEach( function (e,i,a) { units[i] += e; } );
+				}
+			}
+			exp[i] = new qty(Number(split[0]), units);
 		}
 	}
 	
@@ -170,76 +189,33 @@ function processInput (input) {
 }
 
 function power (qty1, qty2) {
-	if (
-		!qty2.units[0] &&
-		!qty2.units[1] &&
-		!qty2.units[2] &&
-		!qty2.units[3] &&
-		!qty2.units[4] &&
-		!qty2.units[5] &&
-		!qty2.units[6]
-	) {
-		return new qty (
-			Math.pow(qty1.value, qty2.value),
-			[
-				qty1.units[0] * qty2.value,
-				qty1.units[1] * qty2.value,
-				qty1.units[2] * qty2.value,
-				qty1.units[3] * qty2.value,
-				qty1.units[4] * qty2.value,
-				qty1.units[5] * qty2.value,
-				qty1.units[6] * qty2.value
-			]
-		);
+	if ( qty2.units.every( function (e,i,a) { return e===0; } ) ) {
+		var units = [];
+		qty1.units.forEach( function (e,i,a) { units.push(e * qty2.value); } );
+		return new qty ( Math.pow(qty1.value, qty2.value), units );
 	} else {
 		raiseError('Cannot raise to power of a dimensioned value');
 	}
 }
 
 function multiply (qty1, qty2) {
-	return new qty (
-		qty1.value * qty2.value,
-		[
-			qty1.units[0] + qty2.units[0],
-			qty1.units[1] + qty2.units[1],
-			qty1.units[2] + qty2.units[2],
-			qty1.units[3] + qty2.units[3],
-			qty1.units[4] + qty2.units[4],
-			qty1.units[5] + qty2.units[5],
-			qty1.units[6] + qty2.units[6]
-		]
-	);
+	var units = [];
+	qty1.units.forEach( function (e,i,a) { units.push(e + qty2.units[i]); } );
+	return new qty ( qty1.value * qty2.value, units );
 }
 
 function divide (qty1, qty2) {
-	if ( qty2.value != 0 ) {
-		return new qty(
-			qty1.value / qty2.value,
-			[
-				qty1.units[0] - qty2.units[0],
-				qty1.units[1] - qty2.units[1],
-				qty1.units[2] - qty2.units[2],
-				qty1.units[3] - qty2.units[3],
-				qty1.units[4] - qty2.units[4],
-				qty1.units[5] - qty2.units[5],
-				qty1.units[6] - qty2.units[6]
-			]
-		);
+	if ( qty2.value !== 0 ) {
+		var units = [];
+		qty1.units.forEach( function (e,i,a) { units.push(e - qty2.units[i]); } );
+		return new qty( qty1.value / qty2.value, units );
 	} else {
 		raiseError('Divide by zero');
 	}
 }
 
 function add (qty1, qty2) {
-	if (
-		qty1.units[0] == qty2.units[0] &&
-		qty1.units[1] == qty2.units[1] &&
-		qty1.units[2] == qty2.units[2] &&
-		qty1.units[3] == qty2.units[3] &&
-		qty1.units[4] == qty2.units[4] &&
-		qty1.units[5] == qty2.units[5] &&
-		qty1.units[6] == qty2.units[6]
-	) {
+	if ( qty2.units.every( function (e,i,a) { return e===qty2.units[i]; } ) ) {
 		return new qty(qty1.value + qty2.value, qty1.units);
 	} else {
 		raiseError('Dimension mismatch');
@@ -252,7 +228,7 @@ function subtract (qty1, qty2) {
 
 function qty(value, units) { // new Dimension
 	this.value = value;
-	if (units == 0) {
+	if (units === 0) {
 		this.units = [0,0,0,0,0,0,0];
 	} else {
 		this.units = units;
@@ -261,22 +237,21 @@ function qty(value, units) { // new Dimension
 		if (arg instanceof qty) {
 			return this.identical(arg);
 		} else if (typeof(arg=='number')) {
-			return this.value == arg
+			return this.value == arg;
 		}
-	}
+	};
 	this.identical = function (arg) {
 		if (arg instanceof qty) {
-			return this.identical(qty2);
 			if (
-				this.value == qty2.value &&
-				this.units[0] == qty2.units[0] &&
-				this.units[1] == qty2.units[1] &&
-				this.units[2] == qty2.units[2] &&
-				this.units[3] == qty2.units[3] &&
-				this.units[4] == qty2.units[4] &&
-				this.units[5] == qty2.units[5] &&
-				this.units[6] == qty2.units[6]
-				// && this.mag == qty2.mag
+				this.value == arg.value &&
+				this.units[0] == arg.units[0] &&
+				this.units[1] == arg.units[1] &&
+				this.units[2] == arg.units[2] &&
+				this.units[3] == arg.units[3] &&
+				this.units[4] == arg.units[4] &&
+				this.units[5] == arg.units[5] &&
+				this.units[6] == arg.units[6]
+				// && this.mag == arg.mag
 			) {
 				return true;
 			} else {
@@ -285,9 +260,9 @@ function qty(value, units) { // new Dimension
 		} else {
 			return false;
 		}
-	}
+	};
 }
-qty.prototype.toString = function () {return String(this.value)};
+qty.prototype.toString = function () { return String(this.value); };
 
 
 function makeNum(id, value, units) {
@@ -307,9 +282,9 @@ function extractVar(s) { // extract variable name from variable string
 }
 
 function writeMessage(msg) {
-	element = document.createElement('p');
+	var element = document.createElement('p');
 	element.innerHTML = msg;
-	box = document.getElementById('scicalcOutput');
+	var box = document.getElementById('scicalcOutput');
 	if (box.children == 0) {
 		box.appendChild(element);
 	} else {
